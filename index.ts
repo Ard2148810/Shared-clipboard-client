6c3e13ab482b710b5460c55cc503937427bf2f34
Adrian Ałaszewski <wns2148810@interia.pl>
2020-11-07 12:07:15 +0100

    Electron run fix

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log("Test");
+import  {app, BrowserWindow} from "electron";
+
+app.on("ready", () => {
+    let win = new BrowserWindow();
+    win.setBounds({ x: 0, y: 0, width: 800, height: 600 });
+});
